6f55f87adf421f1c94df10c482c67cd0b2cc9bb5
Valerie Yeltman <valerieyeltman@Valeries-MacBook-Pro.local>
2021-07-29 23:25:03 -0500

    init project

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Yo Val Pal</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <ul class="text-left">
+        <li class="text-left">connect with git</li>
+        <li class="text-left">create resume</li>
+        <li class="text-left">i mean portfolio</li>
+        <li class="text-left"></li>
+      </ul>
+      <ul class="text-left">
+        <th> class="text-left"to include</th>
+        <li class="text-left">Drupal</li>
+        <li class="text-left">Wordpress</li>
+        <li class="text-left">Email</li>
+        <li class="text-left">Tailwind???</li>
+        <li class="text-left">Marketo</li>
+        <li class="text-left">Mail Chimp</li>
+        <li class="text-left">Bootstrap</li>
+        <li class="text-left">Animation???</li>
+        <li class="text-left">Performance</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
